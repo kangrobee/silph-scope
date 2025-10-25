f5cbc50c37349154bfc24f061a32976d9d63d54f
Robert <kangrobe@berkeley.edu>
2025-10-24 23:03:50 -0700

    Fixed load_sprites to only include sprites that I need, there are still abit of inconsistentices with pokemon that do not have sprites

--- load_sprites.py
+++ load_sprites.py
@@ -1,6 +1,7 @@
 import base64
 import os
 import psycopg2
+import re
 from dotenv import load_dotenv
 
 
@@ -21,12 +22,14 @@ with open("schema.sql", "r", encoding="utf-8") as f:
 folder = "./pokesprites"
 
 for filename in os.listdir(folder):
-    if filename.endswith(".png"):
+    if filename.endswith(".png") and filename[:-4].isdigit() and not filename == '0.png':
+        match = re.fullmatch(r"(\d+)\.png", filename)
+        sprite_id = int(match.group(1))
         path = os.path.join(folder, filename)
         with open(path, "rb") as image_file:
             base64_string = base64.b64encode(image_file.read()).decode("utf-8")
         cur.execute(
-            "INSERT INTO sprites (sprite_name, sprite_data) VALUES (%s, %s)",
-            (filename, base64_string)
+            "INSERT INTO sprites (sprite_id, sprite_name, sprite_data) VALUES (%s, %s, %s)",
+            (sprite_id, filename, base64_string)
         )
 conn.commit()
